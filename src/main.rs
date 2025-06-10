1fcd7a2d38e19200dd21957ca943914d79087716
Mariam Kvantaliani <47283342+mariamikv@users.noreply.github.com>
2025-06-11 01:09:35 +0400

    the type builtin: builtins

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ fn main() {
             println!("{}", format_input(input.trim()));
         } else if "exit 0" == input.trim() {
             break;
+        } else if starts_with_type(&input) {
+            println!("{}", handle_command_type(input.trim()));
         } else {
             println!("{}: command not found", input.trim());
         }
@@ -26,4 +28,20 @@ fn starts_with_echo(s: &str) -> bool {
 
 fn format_input(s: &str) -> String {
     s.replace("echo ", "")
+}
+
+fn starts_with_type(s: &str) -> bool {
+    s.starts_with("type ")
+}
+
+fn handle_command_type(s: &str) -> String {
+    let command = s.split_whitespace().nth(1);
+    match command {
+        Some("echo") | Some("exit") | Some("type") => {
+            format!("{} is a shell builtin", command.unwrap())
+        },
+        _ => {
+            String::from("invalid_command: not found")
+        }
+    }
 }
